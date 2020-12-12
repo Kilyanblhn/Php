a28a11e1abdc6d49ac83af78943e99f17328cdbf
Kilyanblhn <blachon.kilyan@gmail.com>
2020-12-12 14:28:45 +0100

    debut_css

--- views/entete.php
+++ views/entete.php
@@ -1,12 +1,20 @@
-<div class="entete">
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap');
+</style>
 
-    <div class="titre">
+<div style='font-family: "Comfortaa", sans-serif;'>
+
+    <div style="display: flex; justify-content: center;">
         <h1>Bibliothèque de l'IUT2 de GRENOBLE</h1>
     </div>
     <nav>
-        <ul>
-            <li><a href="?page=cd&action=liste">Nos CD</a></li>
-            <li><a href="?page=magazine&action=liste">Nos magazines</a></li>
+        <ul style="list-style: none; display: flex; justify-content: space-around;">
+            <li>
+                <a style="text-decoration: none; color: #50c29c;" href="?page=cd&action=liste"><h3>Disques Compacts</h3></a>
+            </li>
+            <li>
+                <a style="text-decoration: none; color: #608fc2;" href="?page=magazine&action=liste"><h3>Magazines</h3></a>
+            </li>
         </ul>
     </nav>
 </div>
--- views/listeMagazines.php
+++ views/listeMagazines.php
@@ -5,6 +5,8 @@
     <title>Liste de nos magazines</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="views/css/listeMagazines.css">
 </head>
 
@@ -13,16 +15,16 @@
     <div>
         <?php include('views/entete.php'); ?>
     </div>
-    <div class="body">
+    <div class="list-mag-body">
         <div class="contenu">
-            <h2>Liste de nos magazines</h2>
+            <h2 class="clr">Liste de nos magazines</h2>
             <ul>
                 <?php foreach ($magazines as $magazine) {?>
                     <li>
                         <div class="line">
                             <div class="titleCase"> <?= $magazine->titre ?> </div>
-                            <div class="iconCase"> <i class="fa fa-eye" aria-hidden="true"></i> </div>
-                            <div class="iconCase"> <i class="fa fa-times" aria-hidden="true"></i> </div>
+                            <div class="iconCase"> <i class="fa fa-eye clr" aria-hidden="true"></i> </div>
+                            <div class="iconCase"> <i style="color: crimson" class="fa fa-times" aria-hidden="true"></i> </div>
                         </div>
                     </li>
 
